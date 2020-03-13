8c66a7d821e3bd0faccd88abfcb97b12257bfd82
gianscarpe <gianluca@scarpellini.dev>
2020-03-13 11:30:36 +0100

    CNN exp with event frames

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import torch
 from torch.utils.data import Dataset, DataLoader
 from config import MOVEMENTS_PER_SESSION
 import numpy as np
-
+import torch.nn as nn
+import torchvision
+import time
 
 def get_label_from_filename(filepath):
     """Given the filepath of .h5 data, return the correspondent label
@@ -15,20 +17,20 @@ def get_label_from_filename(filepath):
 
     label = 0
     filename = os.path.basename(filepath)
-    session = int(filename[filename.find('session') + len('session')])
-    mov = int(filename[filename.find('mov') + len('mov')])
+    session = int(filename[filename.find('session_') + len('session_')])
+    mov = int(filename[filename.find('mov_') + len('mov_')])
 
-    for i in range(session):
+    for i in range(1, session):
         label += MOVEMENTS_PER_SESSION[i]
 
 
-    return label + mov
+    return label + mov -1
 
 
 class DHP19Dataset(Dataset):
     """Face Landmarks dataset."""
 
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, indexes=None, transform=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
@@ -37,8 +39,8 @@ class DHP19Dataset(Dataset):
                 on a sampletot += n_frames.
         """
 
-        self.x_paths = sorted(glob.glob(os.path.join(root_dir, "*events.h5")))
-        self.x_indexes = np.arange(len(self.x_paths))
+        self.x_paths = sorted(glob.glob(os.path.join(root_dir, "*.npy")))
+        self.x_indexes = indexes if indexes is not None else np.arange(len(self.x_paths))
         self.labels = [get_label_from_filename(x_path) for x_path in self.x_paths]
 
         self.transform = transform
@@ -57,9 +59,97 @@ class DHP19Dataset(Dataset):
         if self.transform:
             x = self.transform(x)
 
+
         return x, y
 
 
-if __name__ == '__main__':
-    root_dir = '/home/gianscarpe/dev/dhp19/data/h5_dataset_7500_events/movements_per_frame'
-    dataloader = DataLoader(DHP19Dataset(root_dir), batch_size=4, shuffle=True, num_workers=4)
+
+class CNN(nn.Module):
+    def __init__(self, input_shape=(260, 346), n_classes=33):
+        super(CNN, self).__init__()
+        self.conv1 = nn.Sequential(         # input shape (1, 250, 346)
+            nn.Conv2d(
+                in_channels=1,              # input depth
+                out_channels=16,            # n_filters
+                kernel_size=5,              # filter size
+                stride=1,                   # filter movement/step
+                padding=2,                  # if want same width and length of this image after onv2d, padding=(kernel_size-1)/2 if stride=
+            ),                              # output shape (16, 28, 28)
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(kernel_size=2),    # choose max value in 2x2 area, output shape (16, 14, 14)
+        )
+        self.conv2 = nn.Sequential(         # input shape (16, 130, 173)
+            nn.Conv2d(16, 32, 5, 1, 2),     # output shape (32, 65, 86)
+            nn.ReLU(),                      # activation
+            nn.MaxPool2d(2),                # output shape (32, 65, 86)
+        )
+        self.out = nn.Linear(32 * 65 * 86, n_classes)   # fully connected layer, output 10 classes
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = x.view(x.size(0), -1)           # flatten the output of conv2 to (batch_size, 32 * 7 * 7)
+        output = self.out(x)
+        return output
+
+
+LR = 0.001
+EPOCH = 10
+
+cnn = CNN()
+cnn.cuda()
+print(cnn)  # net architecture
+
+root_dir = '/home/gianscarpe/dev/dhp19/data/h5_dataset_7500_events/movements_per_frame'
+n_frames = len(os.listdir(root_dir))
+indexes = np.arange(n_frames)
+np.random.shuffle(indexes)
+
+train_index = indexes[:int(.8 * n_frames)]
+val_index = indexes[int(.8 * n_frames):]
+
+train_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor(),
+    torchvision.transforms.Normalize((0.5,), (0.5,)) ])
+
+val_transform = torchvision.transforms.Compose([
+    torchvision.transforms.ToTensor() ])
+
+train_loader = DataLoader(DHP19Dataset(root_dir, train_index, transform=train_transform), batch_size=4, shuffle=True, num_workers=4)
+val_loader = DataLoader(DHP19Dataset(root_dir, val_index, transform=val_transform), batch_size=4, shuffle=True, num_workers=4)
+
+optimizer = torch.optim.Adam(cnn.parameters(), lr=LR)   # optimize all cnn parameters
+loss_func = nn.CrossEntropyLoss()                       # the target label is not one-hotted
+n_train_data = len(train_loader)
+n_val_data = len(val_loader)
+
+STAMP_EVERY = 100
+# training and testing
+for epoch in range(EPOCH):
+
+    start_time = time.time()
+    accumulated_loss = 0
+    for step, (b_x, b_y) in enumerate(train_loader):   # gives batch data, normalize x when iterate train_loader
+
+        b_x = b_x.float()
+
+        output = cnn(b_x.cuda())               # cnn output
+        loss = loss_func(output, b_y.cuda())   # cross entropy loss
+        optimizer.zero_grad()           # clear gradients for this training step
+        loss.backward()                 # backpropagation, compute gradients
+        optimizer.step()                # apply gradients
+
+        accumulated_loss+=loss.cpu().data.numpy()
+        if (step % STAMP_EVERY == 0):
+            print(f"Epoch: {epoch} - [{step/n_train_data:.2f}%] | train loss: {accumulated_loss/(step+1):.4f}")
+
+
+    accuracy = 0
+    for step, (b_x, b_y) in enumerate(val_loader):
+        test_output = cnn(b_x)
+        pred_y = torch.argmax(test_output.cpu().data.numpy(), 1)
+
+        accuracy += float((pred_y == b_y.data.numpy()).astype(int)
+                          .sum())
+    
+    print(f"Epoch: {epoch} | Duration: {start_time - time.time():.4f}s | val accuracy: {accuracy/n_val_data:.4fp}")
