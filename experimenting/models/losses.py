Fixing confs, fixing gradient flow for pixelloss

experimenting/models/losses.py
@@ -28,7 +28,7 @@ class PixelWiseLoss(nn.Module):
     """
     from https://github.com/anibali/margipose/blob/a9dbe5c3151d7a7e071df6275d5702c07ef5152d/src/margipose/models/margipose_model.py#L202
     """
-    def __init__(self, reduction='mask_mean'):
+    def __init__(self, reduction='nanmean'):
         """
         Args:
          reduction (String, optional): only "mask" methods allowed
@@ -46,11 +46,13 @@ class PixelWiseLoss(nn.Module):
         """
         ndims = 2
         n_joints = pred.shape[1]
-        loss = self.divergence(pred, gt, ndims)
-        loss += self.mpjpe(pred, gt)
+        
+        loss = torch.add(self.mpjpe(pred, gt), self.divergence(pred, gt, ndims))
 
-        mask = gt.view(gt.size()[0], -1, n_joints).sum(1) >0
-        return self.reduction(loss, mask)
+        gt_mask = gt.view(gt.size()[0], -1, n_joints).sum(1) >0
+
+        
+        return self.reduction(loss, gt_mask)
         
 
 def _kl(p, q, ndims):
@@ -65,9 +67,15 @@ def _js(p, q, ndims):
     return 0.5 * _kl(p, m, ndims) + 0.5 * _kl(q, m, ndims)
 
 def _get_reduction(reduction_type):
-    switch = {'mean': torch.mean, 'mask_mean': average_loss, 'sum': torch.sum}
+    switch = {'mean': torch.mean, 'mask_mean': average_loss , 'sum': torch.sum}
     
     return switch[reduction_type]
 
+def nanmean(v,  *args, inplace=False, **kwargs):
+    if not inplace:
+        v = v.clone()
+        is_nan = torch.isnan(v)
+        v[is_nan] = 0
+    return v.sum(*args, **kwargs) / (~is_nan).float().sum(*args, **kwargs)
 
 

experimenting/models/metrics.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 from torch import nn
+from torch.nn.functional import mse_loss
 from ..utils import  get_joints_from_heatmap
 from .dsnt import euclidean_losses, dsnt
 from .soft_argmax import SoftArgmax2D
@@ -28,12 +29,14 @@ class MPJPE(BaseMetric):
         p_coords_max = self.soft_argmax(y_pr)
         gt_coords, _ = get_joints_from_heatmap(y_gt)
 
-        dist_2d = euclidean_losses(p_coords_max, gt_coords)
+        #dist_2d = euclidean_losses(p_coords_max, gt_coords)
         
+        gt_mask = y_gt.view(y_gt.size()[0], -1, self.n_joints).sum(1) >0        
+        dist_2d = euclidean_losses(gt_coords, p_coords_max)
+
         if self.reduction:
             # To apply a reduction method (e.g. mean) we need a mask of gt
             # joints
-            
-            mask = y_gt.view(y_gt.size()[0], -1, self.n_joints).sum(1) >0
-            dist_2d = self.reduction(dist_2d, mask)
+
+            dist_2d = self.reduction(dist_2d, gt_mask)
         return dist_2d

train.py
@@ -28,8 +28,11 @@ def main(cfg: DictConfig) -> None:
     ckpt_cb = ModelCheckpoint(filepath=os.path.join(checkpoint_dir, "{epoch:02d}-{val_loss:.2f}"))
 
     profiler = pl.profiler.SimpleProfiler()
+
+    if debug:
+        torch.autograd.set_detect_anomaly(True)
     trainer_configuration = {
-        'gpus':[1], 'benchmark':True, 'max_epochs':cfg.training.epochs,
+        'gpus':cfg.gpus, 'benchmark':True, 'max_epochs':cfg.training.epochs,
         'fast_dev_run':debug,
         'checkpoint_callback':ckpt_cb, 'track_grad_norm':2,
         'weights_summary': 'top', 'logger':logger,