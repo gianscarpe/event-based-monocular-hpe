Fixing nan gradient

experimenting/models/metrics.py
@@ -32,8 +32,9 @@ class MPJPE(BaseMetric):
         #dist_2d = euclidean_losses(p_coords_max, gt_coords)
         
         gt_mask = y_gt.view(y_gt.size()[0], -1, self.n_joints).sum(1) >0        
-        dist_2d = euclidean_losses(gt_coords, p_coords_max)
+        #dist_2d = euclidean_losses(gt_coords, p_coords_max)
 
+        dist_2d = torch.norm((gt_coords - p_coords_max), dim=-1)
         if self.reduction:
             # To apply a reduction method (e.g. mean) we need a mask of gt
             # joints

experimenting/models/module.py
@@ -1,12 +1,12 @@
 import torch
 from torch import nn
 import numpy as np
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 from ..dataset  import get_data, DatasetType
 from .cnns import get_cnn
 import torchvision
 from .metrics import MPJPE
 import torch.nn.functional as F
+
 import hydra
 import pytorch_lightning as pl
 import collections
@@ -184,11 +184,13 @@ class PoseEstimator(BaseModule):
         self.n_channels = hparams.dataset.n_channels
         self.n_joints = hparams.dataset.n_joints
         params = {'n_channels': hparams.dataset['n_channels'],
-                  'n_classes': hparams.dataset['n_joints']}
+                  'n_classes': hparams.dataset['n_joints'],
+                  'encoder_depth':hparams.training.encoder_depth
+        }
 
         self.model = get_cnn(hparams.training.model, params)
 
-        self.metrics = {"IOU" : smp.utils.metrics.IoU(threshold=0.5), "MPJPE":MPJPE(reduction=average_loss)}
+        self.metrics = {"MPJPE":MPJPE(reduction=average_loss)}
 
     def forward(self, x):
         x = self.model(x)
@@ -199,7 +201,6 @@ class PoseEstimator(BaseModule):
         
         output = self.forward(b_x)               # cnn output
         loss = self.loss_func(output, b_y)   # cross entropy loss
-
         logs = {"loss":loss}
         return {"loss":loss, "log":logs}
 

train.py
@@ -2,6 +2,7 @@ import os
 import hydra
 from omegaconf import DictConfig
 import experimenting
+import torch
 
 import pytorch_lightning as pl
 from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
@@ -17,7 +18,7 @@ def main(cfg: DictConfig) -> None:
 
     exp_path = os.getcwd()
     log_path, cur_dir = os.path.split(exp_path)
-    logger = TensorBoardLogger(os.path.join(log_path, "tb_logs"),
+    logger = TensorBoardLogger(os.path.join(cur_dir, "tb_logs"),
                                version=cur_dir)
 
     
@@ -32,10 +33,11 @@ def main(cfg: DictConfig) -> None:
     if debug:
         torch.autograd.set_detect_anomaly(True)
     trainer_configuration = {
-        'gpus':cfg.gpus, 'benchmark':True, 'max_epochs':cfg.training.epochs,
+        'gpus':[1], 'benchmark':True, 'max_epochs':cfg.training.epochs,
         'fast_dev_run':debug,
         'checkpoint_callback':ckpt_cb, 'track_grad_norm':2,
         'weights_summary': 'top', 'logger':logger,
+        'gradient_clip_val':1.5,
         'profiler':profiler}
 
     if cfg.training.early_stopping > 0:
@@ -50,11 +52,12 @@ def main(cfg: DictConfig) -> None:
 
     model = getattr(experimenting, cfg.training.module)(cfg)
     
-    if cfg.training.load_training:
+    if cfg.load_path:
         print('Loading training')
         model = getattr(experimenting,
-                        cfg.training.module).load_from_checkpoint(cfg.training.load_path)
-        trainer_configuration['resume_from_checkpoint'] = cfg.training.load_path
+                        cfg.training.module).load_from_checkpoint(cfg.load_path)
+    if cfg.resume:
+        trainer_configuration['resume_from_checkpoint'] = cfg.load_path
         
     trainer = pl.Trainer(**trainer_configuration)
     trainer.fit(model)