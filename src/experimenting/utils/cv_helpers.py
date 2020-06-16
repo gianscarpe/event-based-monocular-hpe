8e7224de20ef338330f878c579decfeccf5bc012
Gianluca <gianluca@scarpellini.dev>
2020-06-16 14:29:52 +0200

    margipose notebook for 3d plotting

--- src/experimenting/utils/cv_helpers.py
+++ src/experimenting/utils/cv_helpers.py
@@ -1,14 +1,19 @@
 import numpy as np
 import scipy
 import torch
-from pose3d_utils.skeleton_normaliser import SkeletonNormaliser
-from pose3d_utils.camera import CameraIntrinsics
-from .dsntnn import dsnt
-import cv2
+from matplotlib import pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
+import cv2
+from pose3d_utils.camera import CameraIntrinsics
+from pose3d_utils.skeleton_normaliser import SkeletonNormaliser
+
+from .dsntnn import dsnt
 
 __all__ = [
-    'decay_heatmap', 'get_heatmaps', 'get_joints_from_heatmap', 'predict_xyz'
+    'decay_heatmap', 'get_heatmaps', 'get_joints_from_heatmap', 'predict_xyz',
+    'plot_skeleton_2d', 'plot_skeleton_3d', 'plot_heatmap',
+    'decompose_projection_matrix', 'denormalize_predict'
 ]
 
 _normalizer = SkeletonNormaliser()
@@ -48,14 +53,14 @@ def get_heatmaps(vicon_xyz, p_mat, width, height):
     u = u.astype(np.int32)
     v = v.astype(np.int32)
     joints = np.stack((v, u), axis=-1)
-    #hms = _get_heatmap((u, v), mask, height, width, num_joints)
+    hms = _get_heatmap((u, v), mask, height, width, num_joints)
 
     # Get xyz w.r.t. camera coord system
     xyz_cam = M.dot(vicon_xyz_homog)
     # Note: cam coord system is left-handed; Z is along the negative axis
     xyz_cam[2, :] = -xyz_cam[2, :]
     z_ref = xyz_cam[2, 5]
-    return xyz_cam, joints, mask, camera, z_ref
+    return xyz_cam, joints, mask, camera, z_ref, hms
 
 
 def _get_heatmap(joints, mask, heigth, width, num_joints):
@@ -114,8 +119,33 @@ def predict_xyz(out):
     return torch.cat([x, y, z], -1)
 
 
-def denormalize_skeleton(self, normalized_skeleton, camera, z_ref, height,
-                         width):
+def _skeleton_height(skeleton):
+    return skeleton[5, 2]
+
+
+def denormalize_predict(pred, height, width, camera):
+
+    # skeelton
+    homog = torch.cat([pred, torch.ones((13, 1), dtype=pred.dtype)], axis=-1)
+    camera = CameraIntrinsics(camera)
+    z_ref = _normalizer.infer_depth(homog, _skeleton_height, camera, height, width)
+    pred_skeleton = _normalizer.denormalise_skeleton(homog, z_ref, camera, height, width)
+    pred_skeleton = pred_skeleton.narrow(-1, 0, 3).numpy().swapaxes(0, 1)
+    return pred_skeleton
+
+
+def reproject_skeleton(M, joints, inv=1):
+    j = joints.copy()
+    j[2, :] *= inv
+
+    gt = np.matmul(np.linalg.pinv(M), j)
+    gt = gt / gt[3, :]
+    gt = gt[:3, :].swapaxes(0, 1)
+    return gt
+
+
+def project_onto_image(self, normalized_skeleton, camera, z_ref, height,
+                       width):
     homog = torch.cat([
         normalized_skeleton,
         torch.ones((13, 1),
@@ -124,7 +154,7 @@ def denormalize_skeleton(self, normalized_skeleton, camera, z_ref, height,
     ],
                       axis=1)
 
-    skeleton = self.normalizer.denormalise_skeleton(homog, z_ref,
+    skeleton = _normalizer.denormalise_skeleton(homog, z_ref,
                                                     CameraIntrinsics(camera),
                                                     self.height,
                                                     self.width).narrow(
@@ -145,3 +175,120 @@ def denormalize_skeleton(self, normalized_skeleton, camera, z_ref, height,
     joints[:, 0] = coords[:, 1]
     joints[:, 1] = coords[:, 0]
     return joints
+
+
+def plot_heatmap(img):
+    fig, ax = plt.subplots(ncols=img.shape[0], nrows=1, figsize=(20, 20))
+    for i in range(img.shape[0]):
+        ax[i].imshow(img[i])
+        ax[i].axis('off')
+    plt.show()
+
+
+def plot_skeleton_2d(dvs_frame, sample_gt, sample_pred):
+    " To plot image and 2D ground truth and prediction "
+    plt.figure()
+    plt.imshow(dvs_frame, cmap='gray')
+    plt.plot(sample_gt[:, 1], sample_gt[:, 0], '.', c='red', label='gt')
+    plt.plot(sample_pred[:, 1], sample_pred[:, 0], '.', c='blue', label='pred')
+    plt.legend()
+
+
+def plot_skeleton_3d(gt, pred, M):
+    gt = reproject_skeleton(M, gt, -1)
+    pred = reproject_skeleton(M, pred, -1)
+    fs = 5
+    fig = plt.figure(figsize=(fs, fs))
+
+    ax = Axes3D(fig)
+    plot_3d(ax, gt, c='red')
+    plot_3d(ax, pred, c='blue')
+
+
+def get_skeleton_lines(x, y, z):
+    " Definition of skeleton edges "
+    # rename joints to identify name and axis
+    x_head, x_shoulderR, x_shoulderL, x_elbowR, x_elbowL, x_hipR, x_hipL, \
+        x_handR, x_handL, x_kneeR, x_kneeL, x_footR, x_footL = x[0], x[1], x[2], x[3], \
+            x[4], x[5], x[6], x[7], x[8], x[9], x[10], x[11], x[12]
+
+    y_head, y_shoulderR, y_shoulderL, y_elbowR, y_elbowL, y_hipR, y_hipL, \
+        y_handR, y_handL, y_kneeR, y_kneeL, y_footR, y_footL = y[0], y[1], y[2], y[3], \
+            y[4], y[5], y[6], y[7], y[8], y[9], y[10], y[11], y[12]
+
+    z_head, z_shoulderR, z_shoulderL, z_elbowR, z_elbowL, z_hipR, z_hipL, \
+        z_handR, z_handL, z_kneeR, z_kneeL, z_footR, z_footL = z[0], z[1], z[2], z[3],\
+            z[4], z[5], z[6], z[7], z[8], z[9], z[10], z[11], z[12]
+
+    # definition of the lines of the skeleton graph
+    skeleton = np.zeros((14, 3, 2))
+    skeleton[0, :, :] = [[x_head, x_shoulderR], [y_head, y_shoulderR],
+                         [z_head, z_shoulderR]]
+    skeleton[1, :, :] = [[x_head, x_shoulderL], [y_head, y_shoulderL],
+                         [z_head, z_shoulderL]]
+    skeleton[2, :, :] = [[x_elbowR, x_shoulderR], [y_elbowR, y_shoulderR],
+                         [z_elbowR, z_shoulderR]]
+    skeleton[3, :, :] = [[x_elbowL, x_shoulderL], [y_elbowL, y_shoulderL],
+                         [z_elbowL, z_shoulderL]]
+    skeleton[4, :, :] = [[x_elbowR, x_handR], [y_elbowR, y_handR],
+                         [z_elbowR, z_handR]]
+    skeleton[5, :, :] = [[x_elbowL, x_handL], [y_elbowL, y_handL],
+                         [z_elbowL, z_handL]]
+    skeleton[6, :, :] = [[x_hipR, x_shoulderR], [y_hipR, y_shoulderR],
+                         [z_hipR, z_shoulderR]]
+    skeleton[7, :, :] = [[x_hipL, x_shoulderL], [y_hipL, y_shoulderL],
+                         [z_hipL, z_shoulderL]]
+    skeleton[8, :, :] = [[x_hipR, x_kneeR], [y_hipR, y_kneeR],
+                         [z_hipR, z_kneeR]]
+    skeleton[9, :, :] = [[x_hipL, x_kneeL], [y_hipL, y_kneeL],
+                         [z_hipL, z_kneeL]]
+    skeleton[10, :, :] = [[x_footR, x_kneeR], [y_footR, y_kneeR],
+                          [z_footR, z_kneeR]]
+    skeleton[11, :, :] = [[x_footL, x_kneeL], [y_footL, y_kneeL],
+                          [z_footL, z_kneeL]]
+    skeleton[12, :, :] = [[x_shoulderR,
+                           x_shoulderL], [y_shoulderR, y_shoulderL],
+                          [z_shoulderR, z_shoulderL]]
+    skeleton[13, :, :] = [[x_hipR, x_hipL], [y_hipR, y_hipL], [z_hipR, z_hipL]]
+    return skeleton
+
+
+def plot_3d(ax,
+            y_true_pred,
+            c='red',
+            limits=[[-500, 500], [-500, 500], [0, 1500]],
+            plot_lines=True):
+    " 3D plot of single frame. Can be both label or prediction "
+    x = y_true_pred[:, 0]
+    y = y_true_pred[:, 1]
+    z = y_true_pred[:, 2]
+    ax.scatter(x, y, z, zdir='z', s=20, c=c, marker='o', depthshade=True)
+
+    lines_skeleton = get_skeleton_lines(x, y, z)
+    if plot_lines:
+        for line in range(len(lines_skeleton)):
+            ax.plot(lines_skeleton[line, 0, :],
+                    lines_skeleton[line, 1, :],
+                    lines_skeleton[line, 2, :],
+                    c,
+                    label='gt')
+
+    ax.set_xlabel('X Label')
+    ax.set_ylabel('Y Label')
+    ax.set_zlabel('Z Label')
+
+    x_limits = limits[0]
+    y_limits = limits[1]
+    z_limits = limits[2]
+    x_range = np.abs(x_limits[1] - x_limits[0])
+    x_middle = np.mean(x_limits)
+    y_range = np.abs(y_limits[1] - y_limits[0])
+    y_middle = np.mean(y_limits)
+    z_range = np.abs(z_limits[1] - z_limits[0])
+    z_middle = np.mean(z_limits)
+    # The plot bounding box is a sphere in the sense of the infinity
+    # norm, hence I call half the max range the plot radius.
+    plot_radius = 0.5 * np.max([x_range, y_range, z_range])
+    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
+    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
+    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
