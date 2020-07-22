46d6bd5a83263bf54c58223ad4e9a9ead3038dd4
gianscarpe <gianluca.scarpellini@iit.it>
2020-07-22 16:52:41 +0200

    Events simulator for NTU

--- src/experimenting/generator/extractor.py
+++ src/experimenting/generator/extractor.py
@@ -1,52 +1,104 @@
 import cv2
 import os
-import subprocess
-import torch
-import shutil
-import esim_py
 import numpy as np
-import os
-from matplotlib import pyplot as plt
-import glob
-import sys
+
 from tqdm import tqdm
 
+__all__ = ['MPII_extractor', 'NTU_extractor']
 
-def extract_frames(root_dir, video_files, size, output_dir):
-    for seq, video_file in enumerate(tqdm(video_files)):
-        seq_name = os.path.basename(video_file).split(".")[0]
-        seq_dir_parents = os.path.dirname(video_file).replace(root_dir, "")
-        chair_mask_file = video_file.replace('imageSequence', 'ChairMasks')
-        fg_mask_file = video_file.replace('imageSequence', 'FGmasks')
 
-        vcap_video = cv2.VideoCapture(video_file)
-        vcap_chair = cv2.VideoCapture(chair_mask_file)
-        vcap_fg = cv2.VideoCapture(fg_mask_file)
+class NTU_extractor:
+    def _get_ntu_video_files(input_dir):
+        video_files = []
+        for root, dirs, files in os.walk(input_dir):
+            for f in files:
 
-        seq_dir = os.path.join(output_dir, seq_dir_parents, seq_name)
-        imgs_dir = os.path.join(seq_dir, 'imgs')
-        os.makedirs(imgs_dir, exist_ok=True)
+                if f.endswith('.avi'):
+                    video_files.append(os.path.join(root, f))
+        print(f"Found n {len(video_files)} videos")
+        return video_files
 
-        if vcap_video.isOpened() and vcap_fg.isOpened(
-        ) and vcap_chair.isOpened():
-            fps = int(vcap_video.get(cv2.CAP_PROP_FPS))
+    def extract_frames(self, root_dir, size, output_dir):
 
-            frame_count = int(vcap_video.get(cv2.CAP_PROP_FRAME_COUNT))
-            print("Extracting")
+        video_files = NTU_extractor._get_ntu_video_files(root_dir)
 
-            for id in tqdm(range(frame_count)):
-                # Capture frame-by-frame
+        for seq, video_file in enumerate(tqdm(video_files)):
+            seq_name = os.path.basename(video_file).split(".")[0]
+            seq_dir_parents = os.path.dirname(video_file).replace(root_dir, "")
 
-                _, frame = vcap_video.read()
-                _, chair_mask = vcap_chair.read()
-                _, fg_mask = vcap_fg.read()
+            vcap_video = cv2.VideoCapture(video_file)
 
-                frame = apply_masks_to_frame(frame, [chair_mask, fg_mask])
-                frame = cv2.resize(frame, size)
-                cv2.imwrite(os.path.join(imgs_dir, f'frame{id:07d}.png'), frame)
+            seq_dir = os.path.join(output_dir, seq_dir_parents, seq_name)
+            imgs_dir = os.path.join(seq_dir, 'imgs')
+            os.makedirs(imgs_dir, exist_ok=True)
 
-            with open(os.path.join(seq_dir, 'fps.txt'), 'w') as f:
-                f.write(str(fps))
+            if vcap_video.isOpened():
+                fps = int(vcap_video.get(cv2.CAP_PROP_FPS))
+
+                frame_count = int(vcap_video.get(cv2.CAP_PROP_FRAME_COUNT))
+                print("Extracting")
+
+                for id in tqdm(range(frame_count)):
+                    # Capture frame-by-frame
+                    _, frame = vcap_video.read()
+
+                    frame = cv2.resize(frame, size)
+                    cv2.imwrite(os.path.join(imgs_dir, f'frame{id:07d}.png'),
+                                frame)
+
+                with open(os.path.join(seq_dir, 'fps.txt'), 'w') as f:
+                    f.write(str(fps))
+
+
+class MPII_extractor:
+    def _get_mpii_video_files(input_dir):
+        video_files = []
+        for root, dirs, files in os.walk(input_dir):
+            for f in files:
+                base_dir = os.path.split(root)[-1]
+                if f.endswith('.avi') and base_dir == 'imageSequence':
+                    video_files.append(os.path.join(root, f))
+        print(f"Found n {len(video_files)} videos")
+        return video_files
+
+    def extract_frames(self, root_dir, size, output_dir):
+        video_files = MPII_extractor._get_mpii_video_files(root_dir)
+
+        for seq, video_file in enumerate(tqdm(video_files)):
+            seq_name = os.path.basename(video_file).split(".")[0]
+            seq_dir_parents = os.path.dirname(video_file).replace(root_dir, "")
+            chair_mask_file = video_file.replace('imageSequence', 'ChairMasks')
+            fg_mask_file = video_file.replace('imageSequence', 'FGmasks')
+
+            vcap_video = cv2.VideoCapture(video_file)
+            vcap_chair = cv2.VideoCapture(chair_mask_file)
+            vcap_fg = cv2.VideoCapture(fg_mask_file)
+
+            seq_dir = os.path.join(output_dir, seq_dir_parents, seq_name)
+            imgs_dir = os.path.join(seq_dir, 'imgs')
+            os.makedirs(imgs_dir, exist_ok=True)
+
+            if vcap_video.isOpened() and vcap_fg.isOpened(
+            ) and vcap_chair.isOpened():
+                fps = int(vcap_video.get(cv2.CAP_PROP_FPS))
+
+                frame_count = int(vcap_video.get(cv2.CAP_PROP_FRAME_COUNT))
+                print("Extracting")
+
+                for id in tqdm(range(frame_count)):
+                    # Capture frame-by-frame
+
+                    _, frame = vcap_video.read()
+                    _, chair_mask = vcap_chair.read()
+                    _, fg_mask = vcap_fg.read()
+
+                    frame = apply_masks_to_frame(frame, [chair_mask, fg_mask])
+                    frame = cv2.resize(frame, size)
+                    cv2.imwrite(os.path.join(imgs_dir, f'frame{id:07d}.png'),
+                                frame)
+
+                with open(os.path.join(seq_dir, 'fps.txt'), 'w') as f:
+                    f.write(str(fps))
 
 
 def apply_masks_to_frame(video_frame, masks=None):
--- src/experimenting/generator/upsample.py
+++ src/experimenting/generator/upsample.py
@@ -1,28 +1,44 @@
 import argparse
 import os
 from .upsampling.utils import Upsampler
-import torch
+
 import shutil
 from .config import BASE_DIR, DEVICE
 
-CKPT_FILE = os.path.join(BASE_DIR, 'generator', 'upsampling', 'checkpoint', 'SuperSloMo.ckpt')
+CKPT_FILE = os.path.join(BASE_DIR, 'generator', 'upsampling', 'checkpoint',
+                         'SuperSloMo.ckpt')
+
+
 def upsample(input_dir, output_dir, device=DEVICE):
     if os.path.exists(output_dir) and os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
-    
-    upsampler = Upsampler(
-        input_dir=input_dir,
-        output_dir=output_dir,
-        device=device,
-        ckpt_dir = CKPT_FILE)
+
+    upsampler = Upsampler(input_dir=input_dir,
+                          output_dir=output_dir,
+                          device=device,
+                          ckpt_dir=CKPT_FILE)
+
     upsampler.upsample()
 
 
 def get_flags():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", required=True, help='Path to input directory. See README.md for expected structure of the directory.')
-    parser.add_argument("--output_dir", required=True, help='Path to non-existing output directory. This script will generate the directory.')
-    parser.add_argument("--device", type=str, default="cpu", help='Device to be used (cpu, cuda:X)')
+    parser.add_argument(
+        "--input_dir",
+        required=True,
+        help=
+        'Path to input directory. See README.md for expected structure of the directory.'
+    )
+    parser.add_argument(
+        "--output_dir",
+        required=True,
+        help=
+        'Path to non-existing output directory. This script will generate the directory.'
+    )
+    parser.add_argument("--device",
+                        type=str,
+                        default="cpu",
+                        help='Device to be used (cpu, cuda:X)')
     args = parser.parse_args()
     return args
 
@@ -31,11 +47,11 @@ def main():
     flags = get_flags()
     upsample(flags.input_dir, flags.output_dir, flags.device)
 
-    upsampler = Upsampler(
-        input_dir=flags.input_dir,
-        output_dir=flags.output_dir,
-        device=flags.device)
+    upsampler = Upsampler(input_dir=flags.input_dir,
+                          output_dir=flags.output_dir,
+                          device=flags.device)
     upsampler.upsample()
+
 
 if __name__ == '__main__':
     main()
