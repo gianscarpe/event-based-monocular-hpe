622c8038f28909df7398b13f27bb594328fae841
gianscarpe <gianluca@scarpellini.dev>
2020-12-01 18:42:12 +0100

    infer script

--- experimenting/__init__.py
+++ experimenting/__init__.py
@@ -1,7 +1,11 @@
 """Root package info."""
+from experimenting import utils
+from experimenting import dataset
+from experimenting import agents
 
-__version__ = "0.2"
+__version__ = "0.3"
 __author__ = "Gianluca Scarpellini"
 __author_email__ = "me@scarpellini.dev"
 __license__ = "GPLv3"
 __copyright__ = 'Copyright (c) 2018-2020, %s.' % __author__
+
---
+++ scripts/infer.py
@@ -0,0 +1,17 @@
+import logging
+import argparse
+from experimenting.utils.evaluation_helpers import load_model
+from omegaconf import DictConfig
+import hydra
+logging.basicConfig(level=logging.INFO)
+
+@hydra.main(config_path='../confs/train/', config_name='infer.yaml' )
+def main(cfg: DictConfig) -> None:
+    __import__("pdb").set_trace()
+
+    model = load_model(cfg, test_metrics=[])
+    __import__("pdb").set_trace()
+
+
+if __name__ == '__main__':
+    main()
