bf76ca91fbdd1444fc29f88a668ce60bb7f37eab
gianscarpe <gianluca@scarpellini.dev>
2021-02-24 16:38:09 +0100

    refactoring

--- experimenting/utils/train_helpers.py
+++ experimenting/utils/train_helpers.py
@@ -93,7 +93,7 @@ def get_training_params(cfg: DictConfig):
     return trainer_configuration
 
 
-def load_model(load_path: str, module: str, **kwargs) -> pl.LightningModule:
+def load_model(load_path: str, module: str, **kwargs):
     """
     Main function to load a checkpoint.
     Args:
